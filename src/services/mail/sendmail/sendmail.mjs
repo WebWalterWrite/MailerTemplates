59f39db0472d7ed9aad4d959b71dc2a6c26c7ac3
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-08 21:55:16 +0100

    type: fonctionnel(email) desc: fonction pour l'envoi d'email de bienvenue

---
+++ src/services/mail/sendmail/sendmail.mjs
@@ -0,0 +1,21 @@
+import emailer from '../config';
+
+
+/**
+  * @desc envoi mail de bienvenue
+  * @param string name, email - Valeurs pour envoyer le mail
+  * @return bool - success or failure
+*/
+export const UserWelcome = (name, email) => {
+
+    console.log(email)
+    emailer.sendMail({
+        to: email,
+        subject:`${name} bienvenue à toi`,
+        template:'welcome',
+        context:{
+            name: name
+        }
+
+    })
+}
