87a622a1a517693918ea4c8dd965f06ed8059c80
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-08 16:02:04 +0100

    type: applicatif(email) desc: installation nodemailer handlebars pour le templating

--- app.mjs
+++ app.mjs
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import userEmail from './src/routes/mailing/userEmail';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.use('/', userEmail);
 
 export default app;
