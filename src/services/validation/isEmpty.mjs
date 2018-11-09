d67379ed68c08bdf1583f01caf8efc8af851ca9d
Sammy el dueno <sammyeldueno@imac-de-sammy.home>
2018-11-09 13:17:27 +0100

    type: fonctionnel(user  validation) desc: ajout feat vérification si champ est vide de caractère

---
+++ src/services/validation/isEmpty.mjs
@@ -0,0 +1,10 @@
+ 
+// Vérifier si le champ ne comporte aucun carctère.
+
+export const isEmpty = value => {
+    value === undefined ||
+    value === null ||
+    (typeof(value) === 'object' && Object.keys(value).length === 0 ||
+    typeof(value) === 'string' && value.trim().length === 0 )
+};
+
