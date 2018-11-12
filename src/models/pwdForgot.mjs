e1a08102295ff247ce249d666bb3773f74775c95
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-12 10:28:48 +0100

    desc: ajout sequelize model

---
+++ src/models/pwdForgot.mjs
@@ -0,0 +1,26 @@
+import Sequelize from 'sequelize';
+import sequelize from './config';
+
+const PwdForgot = sequelize.define('resetpwds',{
+    id:{
+        type:Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    token:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    dateOfdemand:{
+        type: Sequelize.DATE,
+        allowNull: false
+    }
+})
+
+
+export default PwdForgot;
