7068895d09a5d4eab8f401faf579bae3663a0e5f
Sammy el dueno <sammyeldueno@imac-de-sammy.home>
2018-11-13 00:14:22 +0100

    add user route

--- app.mjs
+++ app.mjs
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import User from './src/routes/user/user';
+import UserEmail from './src/routes/mailing/userEmail';
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 
-app.use('/', User);
+app.use('/', User, UserEmail);
+
 
 export default app;
