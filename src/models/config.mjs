0d7181c7bef0a9fe1cff9d6271b477b42b58bb59
Sammy el dueno <sammyeldueno@imac-de-sammy.home>
2018-11-13 00:15:20 +0100

    add port config

--- src/models/config.mjs
+++ src/models/config.mjs
@@ -1,11 +1,12 @@
 import Sequelize from 'sequelize';
 import key from '../config/keys';
 
-const {DB, USER, PWD} = key.sqlz;
+const {DB, USER, PWD, PORT=null} = key.sqlz;
 
 const sequelize = new Sequelize (DB, USER, PWD,{
     host:'localhost',
     dialect:'mysql',
+    port:PORT,
     define:{
     timestamps: false
     }
