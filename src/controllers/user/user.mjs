c1d2b4765bf94c1b4754de0068a65c2173fb96e6
Sammy el dueno <sammyeldueno@imac-de-sammy.home>
2018-11-13 00:10:31 +0100

    type:feat(user) desc: ajout fonction pwfForgot pour envoi mail si mdp oublié. Vérifié mail en bdd(ok), enregistrer token en bdd(nok), envoyer mail de réini(ok)

--- src/controllers/user/user.mjs
+++ src/controllers/user/user.mjs
@@ -1,7 +1,33 @@
 import { userPwdForgot } from '../../services/mail/sendmail/sendmail';
+import { findUser } from '../../models/querying/userQuery';
+import { token } from '../../services/crypt/token' ;
 
 export const pwdForgot = async (req, res) => {
-    
+    try{
+        let {email} = req.body;
+
+        // vérifier si l'émail existe
+        const ifUser = await findUser(email, 'email', 'firstname')
+        
+        if(ifUser){
+            // Générer un token
+            let key = await token();
+            
+            // insérer le token en bdd
+            /* code code */
+            
+            // envoyer le mail
+            let { user } = ifUser;
+            const isSend = await userPwdForgot(user,email, key)
+
+            // Vérifier si email envoyé
+            isSend ? res.send({success:'email envoyé'}) : res.send({reject:'erreur rencontrée'}) 
+         }
+    }
+    catch(err){
+        throw err;
+        console.log(err)
+    }
 };
 
 
