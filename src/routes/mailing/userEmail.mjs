23ba23420b6bb5d48f07687b7a237f263ed40033
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-14 01:16:48 +0100

    type:app(routing) desc: ajout route pour mdp réinit mdp

--- src/routes/mailing/userEmail.mjs
+++ src/routes/mailing/userEmail.mjs
@@ -1,5 +1,5 @@
 import router from '../config';
-import { pwdForgot } from '../../controllers/user/user';
+import { pwdForgot, pwdInitialize } from '../../controllers/user/user';
 
 /**
   @desc envoi mail de réinitianilisation du mdp
@@ -7,4 +7,9 @@ import { pwdForgot } from '../../controllers/user/user';
 
 router.post('/user/password/forgot', pwdForgot);
 
+/**
+  @desc réinitianilisation du mdp depuis le lien
+*/
+
+router.get('/user/password/initialize/:token',pwdInitialize )
 export default router;
