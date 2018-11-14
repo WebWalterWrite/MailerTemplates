31649cef135a10423c4739aa709739848de4001f
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-11-14 01:15:42 +0100

    type:feat(auth) desc: ajout fonction pwdInitialize -- changement mot de passe

--- src/controllers/user/user.mjs
+++ src/controllers/user/user.mjs
@@ -1,8 +1,12 @@
 import { userPwdForgot } from "../../services/mail/sendmail/sendmail";
 import { findUser } from "../../models/querying/userQuery";
-import { createEmailToken } from "../../models/querying/tokenQuery";
+import {
+	createEmailToken,
+	retrieveEmailToken
+} from "../../models/querying/tokenQuery";
 import { token } from "../../services/crypt/token";
 import { isValidEmail } from "../../services/validation/validation";
+import moment from "moment";
 
 /**
   @desc Renvoi un email suite à une demande de réinitialisation de mot de passe
@@ -16,17 +20,18 @@ import { isValidEmail } from "../../services/validation/validation";
   @param {string} email -parametre de la fonction WelcomeUser
   @param {string} key - parametre de la fonction token
 */
+
+const msg = {
+	unknow: "une erreur s'est produite, veuillez renouveller votre demande",
+	email: "L'adresse email n'a pas été trouvée",
+	sucess: "Un email vient de vous être envoyé",
+	expired: "Le lien de réinitialisation du mot de passe a expiré"
+};
 export const pwdForgot = async (req, res) => {
 	try {
 		/*
 vérifier le champ email
 */
-
-		let msg = {
-			unknow: "une erreur s'est produite, veuillez renouveller votre demande",
-            email: "L'adresse email n'a pas été trouvée",
-            sucess: "Un email vient de vous être envoyé"
-		};
 
 		let { email } = req.body;
 		let isValid = await isValidEmail(email, "email");
@@ -60,9 +65,45 @@ envoyer le mail
 		// Vérifier si email envoyé
 		if (!isSend) return res.status(500).json({ error: msg.unknow });
 
-		return res.json({sucess:msg.succes});
+		return res.json({ sucess: msg.succes });
 	} catch (e) {
 		throw e;
 		console.log(e.message);
 	}
+};
+
+/**
+  @desc Renvoi un email suite à une demande de réinitialisation de mot de passe
+  @func pwdInitialize -  traite le formulaire 'mot de passe oublié'
+  @func isToken - Vérifie si le token est valide en bdd
+  @func token - créer un token à joindre en paramêtre url
+  @param {object} req.params - contient le token 
+*/
+
+export const pwdInitialize = async (req, res) => {
+    
+	/*
+Vérifier si le token est valide
+*/
+
+	const { token } = req.params; // récupérer le token depuis l'url
+	const isToken = await retrieveEmailToken(token); // récupérer le token en bdd
+
+	if (!isToken) return res.status(400).json({ error: "ko" }); // si token invalid
+
+	/*
+Vérifier si le token n'a pas expiré (fixé à 2H)
+*/
+
+	let tokenDate = moment(isToken);
+	let currentDate = moment(new Date());
+	let duration = moment.duration(currentDate.diff(tokenDate));
+	let limitDate = duration.asHours();
+
+	if (limitDate > 2) return res.status(403).json({ err: msg.expired }); // si token expiré
+
+	/*
+  rediriger vers la page de création de nouveau mdp.
+*/
+	return res.send("autorisé :)"); // token valide.
 };
